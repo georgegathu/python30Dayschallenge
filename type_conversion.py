aead3b75a709cd628541d572aee4606fb5698105
georgegathu <ggeorge.gathu@gmail.com>
2023-04-02 14:51:02 +0300

    Data Type conversion

---
+++ type_conversion.py
@@ -0,0 +1,7 @@
+# TYPE CONVERSION
+x = input ("What is x ? ")
+y = input ("What is y ? ")
+
+z = int(x) + int(y)
+
+print(z)
--- variables.py
+++ variables.py
@@ -2,8 +2,8 @@
     VARIABLES: Container of some value inside your computer memory.
 """
 # Ask user name and say hello to user
+
 name = input("What is yout name? ")
 name = name.strip().title() # Remove whitespace from str & capitalize user names
-
 
 print(f"Hello, {name}")
