12a94fb088e30156b0fdd765caded6183cd27db7
georgegathu <ggeorge.gathu@gmail.com>
2023-04-02 15:15:13 +0300

    Data Type conversion (round of float numbers)

--- type_conversion.py
+++ type_conversion.py
@@ -16,7 +16,17 @@ y = int(input ("What is y ? "))
 print(x + y)
 """
 # DATA TYPE FLOAT (this is a floating number e.g 69.69)
+# VERSION 0
+"""
 x = float(input ("What is x ? "))
 y = float(input ("What is y ? "))
 
 print(x + y)
+"""
+# VERSION 2 (Round off the float number):
+x = float(input ("What is x ? "))
+y = float(input ("What is y ? "))
+
+z = round(x + y) # Rounds of to nearest number
+
+print(z) 
