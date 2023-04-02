216e4b62e837a1b54f44fee26619c24cd85e5ea4
georgegathu <ggeorge.gathu@gmail.com>
2023-04-02 14:06:10 +0300

    Improved say hello program

--- variables.py
+++ variables.py
@@ -3,4 +3,7 @@
 """
 # Ask user name and say hello to user
 name = input("What is yout name? ")
+name = name.strip().title() # Remove whitespace from str & capitalize user names
+
+
 print(f"Hello, {name}")
