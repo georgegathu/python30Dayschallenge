d02100b3ae98356d74abc9a0f7e8e45d5eafb686
georgegathu <ggeorge.gathu@gmail.com>
2023-04-02 15:10:33 +0300

    Data Type conversion (float)

--- type_conversion.py
+++ type_conversion.py
@@ -1,7 +1,22 @@
-# TYPE CONVERSION
+# TYPE CONVERSION 
+
+# VERSION 0 (Datatype Int)
+"""
 x = input ("What is x ? ")
 y = input ("What is y ? ")
 
 z = int(x) + int(y)
 
 print(z)
+"""
+# VERSION 1
+"""
+x = int(input ("What is x ? "))
+y = int(input ("What is y ? "))
+print(x + y)
+"""
+# DATA TYPE FLOAT (this is a floating number e.g 69.69)
+x = float(input ("What is x ? "))
+y = float(input ("What is y ? "))
+
+print(x + y)
