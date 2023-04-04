5a80422a7eba891b5b17f55ca955c66ab7a89e2e
georgegathu <ggeorge.gathu@gmail.com>
2023-04-04 11:25:38 +0300

    A little bit of this and that

---
+++ hello.py
@@ -0,0 +1,20 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+print(45 + 45)   # addition(+)
+print(33 - 3)   # subtraction(-)
+print(5 * 10)   # multiplication(*)
+print(50 / 10)   # division(/)
+print(4 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//) this will nod display any remainders
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Gathu'))             # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Gathu'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
