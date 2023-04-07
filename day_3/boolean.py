907e8132cd587f5c6c78d0daf5df6842d93eecc2
georgegathu <ggeorge.gathu@gmail.com>
2023-04-07 23:26:21 +0300

    Boolean data type

---
+++ day_3/boolean.py
@@ -0,0 +1,6 @@
+# Day 2: 30 Days of python programming
+# boolean: represents one of the two values: True or False.
+# The first letter T for True and F for False
+
+print(True)
+print(False)
---
+++ day_3/operators.py
@@ -0,0 +1,2 @@
+# Day 2: 30 Days of python programming
+
