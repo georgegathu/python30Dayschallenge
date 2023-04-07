a8e115fd946ae023b3c65d1555e03359dc1b4f01
georgegathu <ggeorge.gathu@gmail.com>
2023-04-08 00:35:38 +0300

    Operators In Depth

--- day_3/operators.py
+++ day_3/operators.py
@@ -8,6 +8,7 @@
     -
     &=
     |=
+
 (b) Arithmetic Operators:
     Addition(+): a + b
     Subtraction(-): a - b
@@ -16,7 +17,20 @@
     Modulus(%): a % b
     Floor division(//): a // b
     Exponentiation(**): a ** b
-(c)
+
+(c) Comparison Operators: Used to  to compare two values
+    == Equal
+    != Not Equal
+    >  Greater Than
+    <  Less Than
+    >= Greater than or Equal To
+    <= Less Than or Equal To
+
+(d) Logical Operators: Used to combine conditional statements:
+    and - Returns True if both statements are true
+    or  - Returns True if one of the statements is true
+    not - Returns false if the result is true
+
 """
 print('Addition: ', 44 + 45)        # 96
 print('Subtraction: ', 45 - 35)     # 10
@@ -26,3 +40,15 @@ print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floa
 print ('Division without the remainder: ',7 // 3)   # 2
 print('Modulus: ', 3 % 2)         # 1, Gives the remainder
 print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+
+# Comparison Operators
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+
+#Logical Operators
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
