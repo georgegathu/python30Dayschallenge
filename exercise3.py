55aa2efe68911d4b3db613dbc7e18435d64fd236
georgegathu <ggeorge.gathu@gmail.com>
2023-04-10 14:53:14 +0300

    Exercise Day 3: perimeter of a triangle

--- exercise3.py
+++ exercise3.py
@@ -2,9 +2,17 @@ age = 24
 height = 185.0
 complex_no = 22j
 
-base = int(input("Enter base: "))
-height = int(input("Enter height: "))
+#AREA OF A TRIANGLE
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
 
 area = 0.5 * base * height
-
 print (f"The area of the triangle is, {area}")
+
+#PERIMETER OF A TRINGLE
+side_a = float(input("Enter side a: "))
+side_b = float(input("Enter side b: "))
+side_c = float(input("Enter side c: "))
+
+perimeter = side_a + side_b + side_c
+print (f"The perimeter of the triangle is, {perimeter}")
