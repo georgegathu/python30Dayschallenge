bae5e76723392dfe71e51785f23d6d73236dadac
georgegathu <ggeorge.gathu@gmail.com>
2023-04-13 18:01:07 +0300

    String Escape Sequence

--- day_4/strings.py
+++ day_4/strings.py
@@ -36,3 +36,21 @@ print(len(first_name))  # 6
 print(len(last_name))   # 6
 print(len(first_name) > len(last_name)) # False
 print(len(full_name)) # 12
+
+#4. Escape Sequences in Strings
+'''In Python and other programming languages \ followed by a character is an escape sequence. Let us see the most common escape characters:
+
+\n: new line
+\t: Tab means(8 spaces)
+\\: Back slash
+\': Single quote (')
+\": Double quote (")
+'''
+print('I hope everyone is enjoying the Python Challenge.\nAre you ?') # line break
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces 
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+print('This is a backslash  symbol (\\)') # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
