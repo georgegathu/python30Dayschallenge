acebbb77631ef805054056ae7ff9a77763268a32
georgegathu <ggeorge.gathu@gmail.com>
2023-04-16 22:06:18 +0300

    Lists: Accessing List Items Using Positive Indexing

--- day_5/lists.py
+++ day_5/lists.py
@@ -6,7 +6,7 @@ List: is a collection which is ordered and changeable(modifiable). Allows duplic
 Tuple: is a collection which is ordered and unchangeable or unmodifiable(immutable). Allows duplicate members.
 Set: is a collection which is unordered, un-indexed and unmodifiable, but we can add new items to the set. Duplicate members are not allowed.
 Dictionary: is a collection which is unordered, changeable(modifiable) and indexed. No duplicate members.
-
+NB: Lists can have items of different data types
     # syntax
     lst = []
 """
@@ -28,3 +28,16 @@ print('Web technologies:', web_techs)
 print('Number of web technologies:', len(web_techs))
 print('Countries:', countries)
 print('Number of countries:', len(countries))
+
+
+# Accessing List Items Using Positive Indexing
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[0] # we are accessing the first item using its index
+print(first_fruit)      # banana
+second_fruit = fruits[1]
+print(second_fruit)     # orange
+last_fruit = fruits[3]
+print(last_fruit) # lemon
+# Last index
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]
