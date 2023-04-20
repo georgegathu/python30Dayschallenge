e99aa6b96a544016f2a1e6afeb2da09f37313791
georgegathu <ggeorge.gathu@gmail.com>
2023-04-20 13:08:02 +0300

    Getting Set lenth

---
+++ day_6/sets.py
@@ -0,0 +1,20 @@
+# SETS
+"""
+    -Set: Set is a collection of items.
+    -Set is used to store unique items, and it is possible to find the union, intersection, difference, symmetric difference, subset, super set and disjoint set among sets.
+    -Creating a Set:
+    We use curly brackets, {} to create a set or the set() built-in function.
+    =empty set:
+    # syntax
+        st = {}
+        # or
+        st = set()
+    -A set example:
+    fruits = {'banana', 'orange', 'melon', 'lemon'}
+"""
+
+# Getting Set's Length
+# We use len() method to find the length of a set.
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+len(fruits)
+print(len(fruits))
